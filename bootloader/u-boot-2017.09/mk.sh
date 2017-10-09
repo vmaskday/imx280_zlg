907ff992dd53879cabb164939426bd9a6b8380f5
Yangfeng <yangfeng1235@foxmail.com>
2017-10-09 13:43:37 +0800

    optimization cscope
    利用readelf读取elf中调试信息，然后利用调试信息生成cscope.files
    这样用cscope生成的cscope.out就只包含使用的代码的索引

--- bootloader/u-boot-2017.09/mk.sh
+++ bootloader/u-boot-2017.09/mk.sh
@@ -5,5 +5,9 @@ make ARCH=arm  CROSS_COMPILE=${cross} O=OUT mx28evk_defconfig
 make ARCH=arm  CROSS_COMPILE=${cross} O=OUT u-boot.sb  -j4
 cp ./OUT/spl/u-boot-spl bootstream
 cp ./OUT/u-boot bootstream
+cp ./OUT/u-boot ./OUT/u-boot.elf #for Ozone debug
+cp ./OUT/u-boot ../imx-bootlets-src-10.12.01/
 #elftosb -zf imx28 -c arch/arm/cpu/arm926ejs/mxs/u-boot-imx28.bd
-cd bootstream;./elftosb -f imx28 -c uboot_ivt.bd -o boot.sb
+cd bootstream;./elftosb -f imx28 -c uboot_ivt.bd -o boot.sb;cd -
+
+${cross}readelf -wL OUT/u-boot |grep "CU:" |sed -e 's/CU: .//g' -e 's/:$//g' > cscope.files
