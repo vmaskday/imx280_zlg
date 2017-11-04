00ae0aed2ec6323efe0f0bfa58e1f34ab2e99f86
cm-yangfeng <cm-yangfeng@wingtech.com>
2017-11-04 16:42:10 +0800

    update mk.sh

--- mk.sh
+++ mk.sh
@@ -2,42 +2,7 @@
 
 TOP_DIR=`pwd`
 
-build_bootloader() {
-	echo ************ build $0 start **************
-	cd ${TOP_DIR}
-	cd bootloader/u-boot-2009.08 
-	echo 1| ./build-uboot
-	echo 2| ./build-uboot
-	echo 3| ./build-uboot
-}
-build_kernel() {
-	echo ************ build $0 start **************
-	cd ${TOP_DIR}
-#	cd kernel/linux
-#	echo 3|./config-kernel
-#	echo 6|./config-kernel
-	cd kernel/linux_stable/
-	make ARCH=arm CROSS_COMPILE=arm-none-eabi- O=${TOP_DIR}/OUT/kernel_4.4 mxs_defconfig
-	make ARCH=arm CROSS_COMPILE=arm-none-eabi- O=${TOP_DIR}/OUT/kernel_4.4 -j8
-}
-build_tools() {
-	echo ************ build $0 start **************
-	cd ${TOP_DIR}
-#	mkdir -p ${TOP_DIR}/tools/
-	make -C imxtools/sbtools/ O=${TOP_DIR}/tools/
-}
-
-build_bootstream() {
-	echo ************ build $0 start **************
-	cd ${TOP_DIR}
-	cd bootloader/imx-bootlets-src-10.12.01/
-	./build
-}
-
-build_tools
-build_kernel
-#cp OUT/kernel/arch/arm/boot/zImage  bootloader/imx-bootlets-src-10.12.01/
-cp OUT/kernel_4.4/arch/arm/boot/zImage  bootloader/imx-bootlets-src-10.12.01/
-build_bootloader
-cp ./OUT/bootloader/u-boot bootloader/imx-bootlets-src-10.12.01/
-build_bootstream
+make all
+cp OUT/uboot/boot.sb OUT/
+cp OUT/kernel/arch/arm/boot/zImage OUT/
+cp OUT/kernel/arch/arm/boot/dts/imx28-evk.dtb OUT/
