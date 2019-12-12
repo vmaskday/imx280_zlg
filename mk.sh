8effdbc7c685b31365a8047bc996587d065d4e26
Yangfeng <yangfeng1235@foxmail.com>
2019-12-13 00:33:42 +0800

    quik command for build kernel

--- mk.sh
+++ mk.sh
@@ -1,8 +1,20 @@
 # !/bin/sh
 
 TOP_DIR=`pwd`
-
-make all
-cp OUT/uboot/boot.sb OUT/
-cp OUT/kernel/arch/arm/boot/zImage OUT/
-cp OUT/kernel/arch/arm/boot/dts/imx28-evk.dtb OUT/
+_cross_path=`cd ./cross_compile/bin/;pwd`
+cross=${_cross_path}/arm-linux-gnueabi-
+OUT_DIR=${TOP_DIR}/OUT
+if [ ! -d "OUT" ]
+then
+	mkdir OUT
+fi
+if [ X$1 == X ]
+then
+	make all
+	cp OUT/uboot/boot.sb OUT/
+	cp OUT/kernel/arch/arm/boot/zImage OUT/
+	cp OUT/kernel/arch/arm/boot/dts/imx28-evk.dtb OUT/
+else
+	make -C kernel/linux-4.19.88/ ARCH=arm CROSS_COMPILE=${cross} O=${OUT_DIR}/kernel $1
+fi
+#${cross}readelf -wL OUT/kernel5/vmlinux |grep "CU:" |sed -e 's/CU: .//g' -e 's/:$//g' > kernel/linux-5.3.7/cscope.files
